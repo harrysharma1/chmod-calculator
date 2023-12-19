6aeec1b5553a0c7224bd46ef33dbe1eba488835e
Harry Sharma <harrysharma1066@gmail.com>
2023-12-19 11:53:53 +0000

    add: workflow and change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="chmod",
-    version="0.0.1",
+    version="0.0.2",
     author="Harry Sharma",
     author_email="harrysharma1066@gmail.com",
     description="Basic chmod conversion tool",
