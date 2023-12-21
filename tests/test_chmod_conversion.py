0f6244b652d1b94d5755b71de6b077e004bf8885
Harry Sharma <harrysharma1066@gmail.com>
2023-12-21 12:35:54 +0000

    update: test names and assertion

--- tests/test_chmod_conversion.py
+++ tests/test_chmod_conversion.py
@@ -2,22 +2,31 @@ import pytest
 from chmod import ChmodConversion
 class TestChmodConversion():
     
+    def setup_method(self):
+        self.a = ChmodConversion()
     
-    def test_octal_to_symbolic(self):
-        # Test converting octal permissions to symbolic permissions
-        a = ChmodConversion()
-        assert a.int_to_perm(755) == "rwxr-xr-x"
-        assert a.int_to_perm(644) == "rw-r--r--"
-        assert a.int_to_perm(777) == "rwxrwxrwx"
-        assert a.int_to_perm(771) == "rwxrwx--x"
     
-    def test_symbolic_to_octal(self):
-        # Test converting symbolic permissions to octal permissions
-        a = ChmodConversion()
-        assert a.perm_to_int("rwxr-xr-x") == 755
-        assert a.perm_to_int("rw-r--r--") == 644
-        assert a.perm_to_int("rwxrwxrwx") == 777
-        assert a.perm_to_int("--x---rwx") == 107
+    def test_int_to_perm(self):
+        assert self.a.int_to_perm(755) == "rwxr-xr-x"
+        assert self.a.int_to_perm(644) == "rw-r--r--"
+        assert self.a.int_to_perm(777) == "rwxrwxrwx"
+        assert self.a.int_to_perm(771) == "rwxrwx--x"
+    
+    def test_perm_to_int(self):
+        assert self.a.perm_to_int("rwxr-xr-x") == 755
+        assert self.a.perm_to_int("rw-r--r--") == 644
+        assert self.a.perm_to_int("rwxrwxrwx") == 777
+        assert self.a.perm_to_int("--x---rwx") == 107
+    
+    def test_error_handling_int_to_perm(self):
+        assert self.a.int_to_perm(182) == "Incorrect value"
+        assert self.a.int_to_perm(912) == "Incorrect value"
+        assert self.a.int_to_perm(178) == "Incorrect value"
+        assert self.a.int_to_perm(888) == "Incorrect value"
+    
+    def test_error_handling_perm_to_int(self):
+        assert self.a.perm_to_int("") == "Empty string"
+        assert self.a.perm_to_int("rwxrw") == "Incorrect length"
 
 if __name__ == '__main__':
     pytest.main()
